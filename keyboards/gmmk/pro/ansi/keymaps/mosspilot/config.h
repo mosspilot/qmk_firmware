cab1e9e3a4095a758b4844402cb8f635957cb10b
Jeff Moss <10360575+mosspilot@users.noreply.github.com>
2022-02-17 22:42:49 -0800

    Adding my own setup

---
+++ keyboards/gmmk/pro/ansi/keymaps/mosspilot/config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+/* Enable keypress reactive lighting */
+#define RGB_MATRIX_KEYPRESSES // reacts to keypresses
+#define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
